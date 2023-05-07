3b53b5ac6c78f77af18ee1002eae30cf90fc1d13
Teodora Vodopija <115836182+TeodoraVodopija@users.noreply.github.com>
2023-05-07 09:31:00 +0200

    player movement

--- Assets/Scripts/PlayerMovement2D.cs
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement2D : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed; // player speed
     public float jumpForce;
 
     public int jumpsAmount;
     int jumpsLeft;
-    public Transform GroundCheck;
+    public Transform GroundCheck; // circle collider
     public LayerMask GroundLayer;
 
-    bool isGrounded;
+    bool isGrounded; 
 
     float moveInput;
     Rigidbody2D rb2d;
     float scaleX;
-    // Start is called before the first frame update
+
     void Start()
     {
+        // on start - fetching player
         rb2d = GetComponent<Rigidbody2D>();
         scaleX = transform.localScale.x;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
         Jump();
-
     }
 
     private void FixedUpdate()
@@ -45,10 +45,12 @@ public class PlayerMovement2D : MonoBehaviour
 
     public void Flip()
     {
+        // moving to the right
         if (moveInput > 0)
         {
             transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
         }
+        // moving to the left
         if (moveInput < 0)
         {
             transform.localScale = new Vector3((-1) * scaleX, transform.localScale.y, transform.localScale.z);
@@ -57,31 +59,31 @@ public class PlayerMovement2D : MonoBehaviour
 
     public void Jump()
     {
-
+        // check if space tab is hit
         if (Input.GetKeyDown(KeyCode.Space))
         {
             CheckIfGrounded();
             if (jumpsLeft > 0)
             {
                 rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
-                jumpsLeft--;
+                jumpsLeft--; // decreasing jumps
             }
-
         }
-
     }
 
     public void CheckIfGrounded()
     {
+        // is grounded when circle collider detect collision
         isGrounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheck.GetComponent<CircleCollider2D>().radius, GroundLayer);
         ResetJumps();
     }
 
     public void ResetJumps()
     {
+        // when hits the ground, number of jumps reset
         if (isGrounded)
         {
-            jumpsLeft = jumpsAmount;// jumpsAmount =2;
+            jumpsLeft = jumpsAmount;
         }
     }
 }
